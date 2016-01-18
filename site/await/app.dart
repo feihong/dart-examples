c30b83583c29510622b367abddddabc493cfeaa9
Feihong Hsu <feihong.hsu@gmail.com>
2016-01-18 11:29:29 -0600

    await example: slowly count to 5

--- site/await/app.dart
+++ site/await/app.dart
@@ -1,14 +1,20 @@
 import 'dart:html';
+import 'dart:async';
 import 'dart:convert';
 
 main() {
-  for (var n in getNumbers(88, 102)) {
-    print(n);
-  }
+  doSomeStuff().then((result) {
+    print('Done! Result: ${result}');
+  });
 }
 
 void doSomeStuff() async {
+  addPara('Slowly count to 5...');
 
+  for (var n in getNumbers(1, 6)) {
+    addPara(n);
+    await sleep(1);
+  }
 }
 
 void addPara(String mesg) {
@@ -25,4 +31,8 @@ getNumbers(start, end) sync* {
   for (var i=start; i < end; i++) {
     yield i;
   }
+}
+
+sleep(int seconds) {
+  return new Future.delayed(new Duration(seconds: 1));
 }
